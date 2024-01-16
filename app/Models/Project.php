56c1c6160ad81f7eb57da53fa78f149dde0662ee
Daniele Giuntoli <139512577+Finnedorn@users.noreply.github.com>
2024-01-16 09:53:08 +0100

    Initial commit

---
+++ app/Models/Project.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'project_title', 'repo_name','repo_link','slug','preview','description'];
+}
